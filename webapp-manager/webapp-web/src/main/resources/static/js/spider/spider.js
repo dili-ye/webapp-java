9add2105d964db7d2063762a2ebe3bf334ac7766
F-Monkey <291257658@qq.com>
2018-09-04 07:31:18 -0700

    modify

--- webapp-manager/webapp-web/src/main/resources/static/js/spider/spider.js
+++ webapp-manager/webapp-web/src/main/resources/static/js/spider/spider.js
@@ -7,30 +7,80 @@ function sendUrl(t) {
 	if (url && url.length > 0) {
 		$.post(basePath + "/" + service + "/findOneTitleMsg", data, function(
 				result) {
-			console.info(result);
 			if (result.status == 200) {
 				var data = JSON.parse(result.data);
+				console.info(data);
 				var imgSrc = basePath
 						+ "file/download?serviceType=default&picPath="
 						+ encodeURI(data.picPath);
-				var img = new Image();
-				img.src = imgSrc;
-				img.onload = function() {
-					console.log("load img .....src:" + this.src);
-					if (this.width > 0 && this.height > 0) {
-						var scale = 250 / this.width;
-						var width = 250;
-						var height = this.height * scale;
-						$("#wordcloud").append("<img src='" + imgSrc + "' width='" + width
-										+ "px' height='" + height + "px'/>");
-					} else {
-						this.src = imgSrc;
-					}
-				}
+				loadUserData(data.data);
+				loadImg(imgSrc);
 			} else {
 				alert("error!!!");
 			}
 			$(t).attr("onblur", "sendUrl(this)");
 		});
+	}
+}
+
+function loadUserData(data) {
+	var index = 0;
+	var table = $("#userTable");
+	for (k in data) {
+		var tr;
+		if (index % 3 == 0) {
+			tr = $("<tr></tr>");
+			table.append(tr);
+		}
+		var td = $("<td></td>");
+		var user = JSON.parse(k);
+		var content = data[k];
+		var user_div = $("<div></div>");
+		var img = $("<img src='" + user.head + "'/>");
+		var a = $("<a></a>");
+		a.append(img);
+		var username = $("<a></a>");
+		username.text(user.name);
+		username.attr("href", user.url);
+		username.css("color", user.sex == "female" ? "red" : "blue");
+		user_div.append(a);
+		user_div.css("border", user.sex == "female" ? "3px solid red"
+				: "3px solid blue");
+		var titleCount = $("<span></span>");
+		var ul = $("<ul></ul>");
+		var li_titleCount = $("<li></li>");
+		li_titleCount.text("发言:");
+		var li_tbAge = $("<li></li>");
+		li_tbAge.text("吧龄:");
+		ul.append(li_titleCount);
+		ul.append(li_tbAge);
+		titleCount.text(user.tbTitleCount);
+		li_titleCount.append(titleCount);
+		user_div.append(ul);
+		var tbAge = $("<span></span>");
+		tbAge.text(user.tbAge);
+		li_tbAge.append(tbAge);
+		user_div.append(username);
+		td.append(user_div);
+		tr.append(td);
+		index++;
+	}
+}
+
+function loadImg(imgSrc) {
+	var img = new Image();
+	img.src = imgSrc;
+	img.onload = function() {
+		console.log("load img .....src:" + this.src);
+		if (this.width > 0 && this.height > 0) {
+			var width = 500;
+			var scale = width / this.width;
+			var height = this.height * scale;
+			$("#wordcloud").append(
+					"<img src='" + imgSrc + "' width='" + width
+							+ "px' height='" + height + "px'/>");
+		} else {
+			this.src = imgSrc;
+		}
 	}
 }
