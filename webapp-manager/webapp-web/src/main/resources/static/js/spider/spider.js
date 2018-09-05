15801dbca3be63d94d7fa71ad70ddd3d8524e4c0
F-Monkey <291257658@qq.com>
2018-09-05 15:58:59 -0700

    modify

--- webapp-manager/webapp-web/src/main/resources/static/js/spider/spider.js
+++ webapp-manager/webapp-web/src/main/resources/static/js/spider/spider.js
@@ -1,80 +1,38 @@
 function sendUrl(t) {
 	var url = $(t).val();
 	var func = $(t).removeAttr("onblur");
+	var actionType = $(t).attr("action-Type");
 	var data = {
 		"url" : url
 	};
 	if (url && url.length > 0 && JSON.stringify(result_data_all) == "{}") {
-		$.post(basePath + "/" + service + "/baiduTieba", data,
-				function(result) {
-					if (result && result.status == 200) {
-						var data = JSON.parse(result.data);
-						result_data_all = data;
-						var imgSrc = basePath
-								+ "file/download?serviceType=default&picPath="
-								+ encodeURI(data.picPath);
-						loadUserData(data.data);
-						console.info(imgSrc);
-						loadImg(imgSrc);
-					} else {
-						alert("error!!!");
-					}
-					$(t).attr("onblur", "sendUrl(this)");
-				});
+		$.post(basePath + service + "/" + actionType, data, function(result) {
+			if (result && result.status == 200) {
+				var data = JSON.parse(result.data);
+				result_data_all = data;
+				var imgSrc = basePath
+						+ "file/download?serviceType=default&picPath="
+						+ encodeURI(data.picPath);
+				loadUserData(data.data);
+				loadImg(imgSrc);
+			} else {
+				alert("error!!!");
+			}
+			$(t).attr("onblur", "sendUrl(this)");
+		});
 	}
 }
 
-function loadUserData(data) {
-	var index = 0;
-	var table = $("#userTable");
-	var male_count = 0;
-	var female_count = 0;
-	for (k in data) {
-		var tr;
-		if (index % 3 == 0) {
-			tr = $("<tr></tr>");
-			table.append(tr);
-		}
-		var td = $("<td></td>");
-		var user = JSON.parse(k);
-		var content = data[k];
-		var user_div = $("<div></div>");
-		var img = $("<img src='" + user.head
-				+ "' width='110px' height='110px'/>");
-		var a = $("<a></a>");
-		a.append(img);
-		var username = $("<a></a>");
-		username.text(user.name);
-		username.attr("href", user.url);
-		username.css("color", user.sex == "female" ? "red" : "blue");
-		user_div.append(a);
-		user_div.css("border", user.sex == "female" ? "3px solid red"
-				: "3px solid blue");
-		var titleCount = $("<span></span>");
-		var ul = $("<ul></ul>");
-		var li_titleCount = $("<li></li>");
-		li_titleCount.text("发言:");
-		var li_tbAge = $("<li></li>");
-		li_tbAge.text("吧龄:");
-		ul.append(li_titleCount);
-		ul.append(li_tbAge);
-		titleCount.text(user.tbTitleCount);
-		li_titleCount.append(titleCount);
-		user_div.append(ul);
-		var tbAge = $("<span></span>");
-		tbAge.text(user.tbAge);
-		li_tbAge.append(tbAge);
-		user_div.append(username);
-		td.append(user_div);
-		tr.append(td);
-		if (user.sex == "female") {
-			female_count++;
-		} else {
-			male_count++;
-		}
-		index++;
-	}
+
+var baiduTieba_loader = function(){
+	
+	
+	loader:function(data){
+		
+	},
+	loaderUserData
 }
+
 
 function loadImg(imgSrc) {
 	var img = new Image();
@@ -96,7 +54,7 @@ function loadImg(imgSrc) {
 
 function changeUserTable(t) {
 	var $this = $(t);
-	if ($this.val() == 'show') {
+	if ($this.val() == 'hide') {
 		$("#userTable").css("display", "none");
 	} else {
 		$("#userTable").css("display", "");
